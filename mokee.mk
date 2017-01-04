3d520fab8de5a6f200c7921712f13bd538f249fd
carsonhgr <carsonhgr@gmail.com>
2017-01-04 20:49:39 +0800

    d857: Remove enhanced NFC feature

--- mokee.mk
+++ mokee.mk
@@ -3,9 +3,6 @@ $(call inherit-product, device/lge/d857/full_d857.mk)
 # Inherit some common MK stuff.
 $(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
-
 PRODUCT_NAME := mk_d857
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
